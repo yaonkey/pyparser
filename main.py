ccb86fce929f5ae5220325a9012293f3797e8ada
ak <ak@easy-it.ru>
2021-08-26 16:46:11 +0300

    added selenium support

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from bs4 import BeautifulSoup as bs
 import csv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
+SELENIUM_PATH = ""
 FILENAME = "lesmastyle"
+PRODUCT_TYPE = "Арка"
 
 site_urls = [
     "https://lesmastyle.ru/shop/arki/ellada/",
@@ -14,7 +17,7 @@ site_urls = [
     "https://lesmastyle.ru/shop/arki/kazanka/",
     "https://lesmastyle.ru/shop/arki/milano/",
     "https://lesmastyle.ru/shop/arki/palermo/",
-    "https://lesmastyle.ru/shop/arki/romanskay/",
+    "https://lesmastyle.ru/shop/arki/romanskaya/",
     "https://lesmastyle.ru/shop/arki/ufimka/"
 ]
 
@@ -37,24 +40,45 @@ class SiteParser:
 
     def run(self):
         resp = requests.get(self.url)
-        soup = bs(resp.text, "lxml")
-        print(soup)
-        self.processed_soup = self.__process(soup)
-        self.__save("1")
+        driver = webdriver.Firefox(SELENIUM_PATH)
+        driver.get(self.url)
+        html = driver.page_source
+        self.soup = bs(html, "lxml")
+        self.processed_soup = self.__process()
+        # self.__save()
 
-    def __save(self, name: str):
+    def __save(self):
         csv_file = open("./data/" + FILENAME + ".csv", "a")
         csv_writer = csv.writer(csv_file)
         csv_writer.writerow(self.processed_soup)
 
-    def __process(self, raw):
-        pass
+    def __process(self):
+        self.product["name"] = f"{PRODUCT_TYPE} {self.soup.find('h1', {'class': 'header-img mb-3'}).get_text()}"
+        # self.product["img"] = self.soup.find("div.cat-item__img.mb-4 a", {"data-fancybox": "shop-item-photo",
+        #                                                                   "class": "d-block mb-3"}).get("href")
+        print([img.get("src") for img in self.soup.find_all('img')])
+        # self.product["img"] = [img for img.get('') in self.soup.find_all('a')]
+        # self.product["color"] = self.soup.find("h4", {"class": "font-weight-bold"}).getText()
+        self.product["material"] = self.soup.find()
+        self.product["height"] = self.soup.find()
+        self.product["width"] = self.soup.find()
+        self.product["depth"] = self.soup.find()
+        self.product["sku"] = self.soup.find()
+        self.product["price"] = self.soup.find()
+        print(self.product["name"])
+        print(self.product["img"])
+        # print(self.product["color"])
+        return self.product
 
-    def __str__(self):
-        resp = requests.get(self.url)
-        soup = bs(resp.text, "lxml")
-        return soup
+    def getPageFromUrl(self):
+        return bs(requests.get(self.url).text, "lxml")
 
 
-pars = SiteParser(site_urls[0])  # one element for testing
-pars.run()
+delo = 1
+if delo == 1:
+    for url in site_urls:
+        pars = SiteParser(url)  # one element for testing
+        pars.run()
+elif delo == 0:
+    pars = SiteParser(site_urls[0])  # one element for testing
+    pars.run()
