45433bd59279f9218cfa12765762d73c070c5ad3
ak <ak@easy-it.ru>
2021-08-30 15:32:59 +0300

    Added auto-parsing with selenium (but view https://github.com/yaonkey/pyparser/issues/4#issue-982741692)

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+
+if os.name == "nt":
+    SELENIUM_PATH = "assets\\geckodriver.exe"
+elif os.name == 'posix':
+    SELENIUM_PATH = "./assets/geckodriver"
+else:
+    SELENIUM_PATH = str(input("Enter path to Selenium: "))
+FILENAME = "lesmastyle"
+PRODUCT_TYPE = "Арка"
+DEBUG = True
+IS_BROWSER_HIDE = False
+
+SITE_URLS = [
+    "https://lesmastyle.ru/shop/arki/ellada/",
+    "https://lesmastyle.ru/shop/arki/britanskaya/",
+    "https://lesmastyle.ru/shop/arki/valencia/",
+    "https://lesmastyle.ru/shop/arki/kazanka/",
+    "https://lesmastyle.ru/shop/arki/milano/",
+    "https://lesmastyle.ru/shop/arki/palermo/",
+    "https://lesmastyle.ru/shop/arki/romanskaya/",
+    "https://lesmastyle.ru/shop/arki/ufimka/"
+]
--- main.py
+++ main.py
@@ -6,48 +6,32 @@ from bs4 import BeautifulSoup as bs
 import csv
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-import os
-from time import sleep
-
-if os.name == "nt":
-    SELENIUM_PATH = "assets\\geckodriver.exe"
-elif os.name == 'posix':
-    SELENIUM_PATH = "./assets/geckodriver"
-else:
-    SELENIUM_PATH = str(input("Enter path to Selenium: "))
-FILENAME = "lesmastyle"
-PRODUCT_TYPE = "Арка"
-
-SITE_URLS = [
-    "https://lesmastyle.ru/shop/arki/ellada/",
-    "https://lesmastyle.ru/shop/arki/britanskaya/",
-    "https://lesmastyle.ru/shop/arki/valencia/",
-    "https://lesmastyle.ru/shop/arki/kazanka/",
-    "https://lesmastyle.ru/shop/arki/milano/",
-    "https://lesmastyle.ru/shop/arki/palermo/",
-    "https://lesmastyle.ru/shop/arki/romanskaya/",
-    "https://lesmastyle.ru/shop/arki/ufimka/"
-]
+from time import sleep, time
+from config import *
 
 
 class SiteParser:
     product: dict = {
         "name": '',
         "img": '',
-        "color": [],
-        "material": [],
-        "height": [],
-        "width": [],
-        "depth": [],
-        "sku": [],
-        "price": [],
+        "color": '',
+        "material": '',
+        "height": '',
+        "width": '',
+        "depth": '',
+        "sku": '',
+        "price": '',
     }
+    max_iteration_on_one_material: int
+    __iter: int = 0
+    first_iter: bool = True
 
     def __init__(self, url: str == ""):
+        self.__timer = time()
         if url != "":
             self.url = url
         options = Options()
-        options.headless = True
+        options.headless = IS_BROWSER_HIDE
         self.driver = webdriver.Firefox(options=options, executable_path=rf'{SELENIUM_PATH}')
 
     def run(self, url: str = ""):
@@ -55,57 +39,135 @@ class SiteParser:
             self.url = url
         self.driver.get(self.url)
         sleep(7)
-        html = self.driver.page_source
-        self.soup = bs(html, "lxml")
+        self.__get_root_home_page()
         self.__process()
-        # self.__save()
+        self.__save()
 
     def __save(self):
-        csv_file = open("./data/" + FILENAME + ".csv", "a")
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(self.processed_soup)
+        if DEBUG:
+            print(self.product)
+        with open("./data/" + FILENAME + ".csv", "a") as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(self.product)
 
     def __process(self):
-        product_div = self.soup.find('div', {"class", "home"})
+        product_div = self.__get_root_home_page()
         self.product["name"] = " ".join(f"{product_div.find('h2').get_text()}".split(" ")[0:2])
         self.product[
-            'img'] = f"https://{FILENAME}.ru{[img.get('src') for img in product_div.find_all('img', {'class', 'img-fluid'})][0]}"  # .img-fluid[0]
-        if self.url != "https://lesmastyle.ru/shop/arki/romanskaya/":
-            self.product["color"] = [color.text.strip() for color in product_div.find_all('label', {
-                'for': [mid['id'] for mid in
-                        product_div.find_all("input", {"type": "radio", "class": "custom-control-input"})][3:]})]
-            self.product['material'] = [material.text.strip() for material in product_div.find_all('label', {
-                'for': [mid['id'] for mid in
-                        product_div.find_all("input", {"type": "radio", "class": "custom-control-input"})][0:3]})]
-        else:
-            self.product["color"] = [color.text.strip() for color in product_div.find_all('label', {
-                'for': [mid['id'] for mid in
-                        product_div.find_all("input", {"type": "radio", "class": "custom-control-input"})][4:]})]
-            self.product['material'] = [material.text.strip() for material in product_div.find_all('label', {
-                'for': [mid['id'] for mid in
-                        product_div.find_all("input", {"type": "radio", "class": "custom-control-input"})][0:4]})]
-
-        # self.product["material"]
-        self.product["height"] = [height.text.strip() for height in
-                                  product_div.find("select", {"id": "property_height"}).find_all("option")]
-        self.product["width"] = [width.text.strip() for width in
-                                 product_div.find("select", {"id": "property_width"}).find_all("option")]
-        self.product["depth"] = [depth.text.strip() for depth in
-                                 product_div.find("select", {"id": "property_wall_thickness"}).find_all("option")]
+            'img'] = f"https://{FILENAME}.ru{product_div.find('a', {'data-fancybox': 'shop-item-photo'}).find('img')['src']}"
+        # self.product['material'] = [material.text.strip() for material in product_div.find_all('label', {
+        #     'for': [mid['id'] for mid in
+        #             product_div.find_all("input", {"type": "radio", "class": "custom-control-input"}) if
+        #             'colorType' in mid['id']]})]
+        # self.product["color"] = [color.text.strip() for color in product_div.find_all('label', {
+        #     'for': [mid['id'] for mid in
+        #             product_div.find_all("input", {"type": "radio", "class": "custom-control-input"}) if
+        #             'color' in mid['id'] and 'colorType' not in mid['id']]})]
+        # self.product["height"] = [height.text.strip() for height in
+        #                           product_div.find("select", {"id": "property_height"}).find_all("option")]
+        # self.product["width"] = [width.text.strip() for width in
+        #                          product_div.find("select", {"id": "property_width"}).find_all("option")]
+        # self.product["depth"] = [depth.text.strip() for depth in
+        #                          product_div.find("select", {"id": "property_wall_thickness"}).find_all("option")]
         self.product["sku"] = product_div.find_all("span", {"class": "text-danger"})[0].get_text().split(" ")[1]
-        self.product["price"] = ''.join([elem for elem in product_div.find_all("span", {"class": "text-danger"})[1].get_text().split(" ")[1] if elem != '\xa0'])
+        self.product["price"] = ''.join(
+            [elem for elem in product_div.find_all("span", {"class": "text-danger"})[1].get_text().split(" ")[1] if
+             elem != '\xa0'])
 
-        print(self.product)
+        self.max_iteration_on_one_material = len(self.product["color"]) * len(self.product["height"]) * len(
+            self.product["width"]) * len(self.product["depth"])
+        self.driver.execute_script('''$("div.jivo-iframe-container").remove();''')
+        if self.first_iter:
+            self.__get_next_material_by_click()
         return self.product
 
-    def getPageFromUrl(self):
+    def __get_page_from_url(self):
         return bs(requests.get(self.url).text, "lxml")
 
+    def __get_next_material_by_click(self):
+        self.__get_root_home_page()
+        material_buttons_ids = [material['id'] for material in
+                                self.root_home_page.find_all("input",
+                                                             {"type": "radio", "class": "custom-control-input"}) if
+                                'colorType' in material['id']]
+        self.first_iter = False
+        for element_button in material_buttons_ids:
+            __button = self.driver.find_element_by_css_selector(f"label[for='{element_button}']")
+            self.product['material'] = __button.text
+            __button.click()
+            sleep(7)
+            self.__close_jivo()
+            self.__get_next_color_by_click()
+
+    def __get_next_color_by_click(self):
+        self.__get_root_home_page()
+        color_buttons_ids = [color['id'] for color in
+                             self.root_home_page.find_all("input",
+                                                          {"type": "radio", "class": "custom-control-input"}) if
+                             'colorType' not in color['id'] and 'color' in color['id']]
+        for element_button in color_buttons_ids:
+            __button = self.driver.find_element_by_css_selector(f"label[for='{element_button}']")
+            self.product['color'] = __button.text
+            __button.click()
+            sleep(7)
+            self.__close_jivo()
+            self.__get_next_height_by_click()
+
+# todo: fix bug with empty height selector
+    def __get_next_height_by_click(self):
+        self.__get_root_home_page()
+        height_select_button = self.root_home_page.find("select", {"id": "property_height"})
+        height_buttons_ids = height_select_button.find_all('option')
+        for element_button in height_buttons_ids:
+            __button = self.driver.find_element_by_css_selector(f"""option[value='{element_button["value"]}']""")
+            self.product['height'] = __button.text
+            __button.click()
+            sleep(7)
+            self.__close_jivo()
+            self.__get_next_width_by_click()
+
+    def __get_next_width_by_click(self):
+        self.__get_root_home_page()
+        width_select_button = self.root_home_page.find("select", {"id": "property_width"})
+        width_buttons_ids = width_select_button.find_all('option')
+        for element_button in width_buttons_ids:
+            __button = self.driver.find_element_by_css_selector(f"""option[value='{element_button["value"]}']""")
+            self.product['width'] = __button.text
+            __button.click()
+            sleep(7)
+            self.__close_jivo()
+            self.__get_next_depth_by_click()
+
+    def __get_next_depth_by_click(self):
+        self.__get_root_home_page()
+        depth_select_button = self.root_home_page.find("select", {"id": "property_wall_thickness"})
+        depth_buttons_ids = depth_select_button.find_all('option')
+        for element_button in depth_buttons_ids:
+            __button = self.driver.find_element_by_css_selector(f"""option[value='{element_button["value"]}']""")
+            self.product['depth'] = __button.text
+            __button.click()
+            sleep(7)
+            self.__close_jivo()
+            self.__process()
+
+    def __close_jivo(self):
+        self.driver.execute_script('''$("div.jivo-iframe-container").remove();''')
+        self.driver.execute_script('''$("jdiv").remove();''')
+        sleep(2)
+
+    def __get_root_home_page(self):
+        self.html = self.driver.page_source
+        self.soup = bs(self.html, "lxml")
+        self.root_home_page = self.soup.find('div', {"class", "home"})
+        sleep(1)
+        return self.root_home_page
+
     def __del__(self):
+        print(f"{int(time() - self.__timer)} sec")
         self.driver.quit()
 
 
-delo = 1
+delo = 0
 pars = SiteParser(url="")
 if delo == 1:
     for url in SITE_URLS:
